570cf2c9d604307585a07a765f1c1274ed045c81
Trương Lê Anh Vũ <truongleanhvu20052004@gmail.com>
2025-12-04 14:02:29 +0700

    chore: initialize Edu Core project with Spring Boot and Maven setup

---
+++ src/main/java/com/tlavu/educore/EduCoreApplication.java
@@ -0,0 +1,13 @@
+package com.tlavu.educore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tlavu/educore/EduCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tlavu.educore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
