afaec53b73a7aecb2f58237559a06fbb403837a2
Trương Lê Anh Vũ <truongleanhvu20052004@gmail.com>
2025-12-06 17:27:56 +0700

    fix(config): simplify ObjectMapper configuration by removing unknown properties handling

--- src/main/java/com/tlavu/educore/shared/infrastructure/config/app/AppConfig.java
+++ src/main/java/com/tlavu/educore/shared/infrastructure/config/app/AppConfig.java
@@ -1,6 +1,5 @@
 package com.tlavu.educore.shared.infrastructure.config.app;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -33,7 +32,6 @@ public class AppConfig {
     public ObjectMapper objectMapper() {
         return new ObjectMapper()
                 .registerModule(new JavaTimeModule())
-                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     }
 }
